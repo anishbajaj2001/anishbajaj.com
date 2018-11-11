8558ad241dd1c9c55adab7226816c4f042e8f8cf
anishbajaj2001 <anishbajaj01@gmail.com>
2018-11-11 20:02:21 +0530

    fancier

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+var sentences = ['Semicolons optional.', 'Best viewed in Sublime Text 3.', '#PCMR', 'This text is subject to change.', ];
+var counter = 1;
+var ticker = $("footer .ticker");
+function changeText() {
+    var el = ticker
+      , text = sentences[counter]
+      , oldText = el.text();
+    var x = setInterval(function() {
+        if (oldText.length != 0) {
+            oldText = oldText.slice(0, -1);
+            el.text(oldText);
+        } else {
+            setTimeout(function() {
+                var y = setInterval(function() {
+                    if (el.text().length != text.length) {
+                        el.text(text.slice(0, el.text().length + 1));
+                    } else {
+                        setTimeout(function() {
+                            if (counter >= sentences.length - 1)
+                                counter = 0;
+                            else
+                                counter++;
+                            changeText();
+                        }, 5000);
+                        clearInterval(y);
+                    }
+                }, 60);
+            }, 60);
+            clearInterval(x);
+        }
+    }, 60);
+}
+ticker.html(sentences[0]);
+setTimeout(changeText, 5000);
